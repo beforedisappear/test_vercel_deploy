07a5a2ab64037d8310fbbee235e885f52c442202
beforedisappear <roman23kl@gmail.com>
2024-03-10 19:39:47 +0300

    init

---
+++ src/app/service/page.tsx
@@ -0,0 +1,39 @@
+import { cookies } from "next/headers";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
+interface PageProps {
+  params: { id: string };
+  searchParams: { cafeType: string };
+}
+
+export default async function ServicePage(props: PageProps) {
+  const myCookies = cookies().get("some");
+
+  const data =
+    await fetch(`https://api-dev.xhalal.ru/api/v2/facilities/search?page=0&type=CAFE&cityId=d049b761-2797-40cf-b595-728876e79fc6${
+      props.searchParams.cafeType
+        ? `&cafeType=${props.searchParams.cafeType}`
+        : ""
+    }
+  `);
+
+  if (!data.ok) return notFound();
+
+  const res = await data.json();
+
+  return (
+    <>
+      <Link href={"?cafeType=CAFE"}>кафе</Link>
+      <br />
+      <Link href={"?cafeType=FAST_FOOD"}>перекус</Link>
+      <br />
+      <br />
+      <br />
+
+      {res.facilities.map((el: { id: string; title: string }) => (
+        <div>{el.title}</div>
+      ))}
+    </>
+  );
+}
